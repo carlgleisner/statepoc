c188bc94d78e0a6bb66b71940967656bcc52aa68
Carl Gleisner <carlgleisner@users.noreply.github.com>
2024-02-09 08:14:36 +0100

    Initial commit.

---
+++ lib/statepoc.ex
@@ -0,0 +1,18 @@
+defmodule Statepoc do
+  @moduledoc """
+  Documentation for `Statepoc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Statepoc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/statepoc_test.exs
@@ -0,0 +1,8 @@
+defmodule StatepocTest do
+  use ExUnit.Case
+  doctest Statepoc
+
+  test "greets the world" do
+    assert Statepoc.hello() == :world
+  end
+end
